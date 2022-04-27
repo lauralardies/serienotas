ab25695bf9f83d38abfe603a3c17a48879b8f96e
lauralardies <lrodrlar@myuax.com>
2022-04-27 10:11:35 +0200

    media

--- analisis.py
+++ analisis.py
@@ -1,5 +1,18 @@
 import pandas as pd
 
+# Creamos un DataFrame con Pandas a partir del archivo .csv
 df = pd.read_csv("grades.csv", ",")
 
-print(df)
+# Nuestro dataset tiene 6 "assignments" distintos, por lo que habrá que hacer el mismo cálculo 6 veces, uno de cada assignment. 
+
+# ---- ESTUDIO DE LA TENDENCIA CENTRAL ----
+
+# Primero calculamos la media de cada uno de los exámenes.
+
+media1 = df["assignment1_grade"].mean()
+media2 = df["assignment2_grade"].mean()
+media3 = df["assignment3_grade"].mean()
+media4 = df["assignment4_grade"].mean()
+media5 = df["assignment5_grade"].mean()
+media6 = df["assignment6_grade"].mean()
+
